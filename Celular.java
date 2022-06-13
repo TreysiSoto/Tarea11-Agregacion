943e8281f27fea0ddc2d43f4a4127fbd0673600e
TreysiSoto <103224588+TreysiSoto@users.noreply.github.com>
2022-06-13 16:04:39 -0500

    Create Celular.java

---
+++ Celular.java
@@ -0,0 +1,28 @@
+package Movil;
+
+public class Audifonos {
+    private String marca;
+    private String color;
+
+    public Audifonos(String marca, String color) {
+        this.marca = marca;
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
